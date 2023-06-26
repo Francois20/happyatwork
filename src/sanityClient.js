7da062663c96a7505972694dce3de2798aae8dc0
sandraottersten <otterstensandra@gmail.com>
2023-06-26 14:47:00 +0200

    sanity client id

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -1,15 +1,16 @@
 import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
+import { env } from '$env/dynamic/public';
 
 export const client = sanityClient({
-	projectId: 'szdcwwtw',
+	projectId: env.PUBLIC_SANITY_PROJECT_ID,
 	dataset: 'production',
 	apiVersion: '2022-01-07',
 	useCdn: true
 });
 
-const builder = imageUrlBuilder(client)
+const builder = imageUrlBuilder(client);
 
 export const urlFor = (source) => {
-  return builder.image(source)
-}
+	return builder.image(source);
+};
