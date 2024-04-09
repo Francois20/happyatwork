d69fd82681916a5f33141174add9aff4b2b911ba
Jovan <jovan.14@hotmail.com>
2024-04-09 03:18:36 +0200

    Added missing alt to Link component

--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -1,11 +1,12 @@
 <script>
 	import { IconChevronRight } from '@tabler/icons-svelte';
 
-	export let link;
+	export let link, alt = null;
 </script>
 
 <a
 	class="font-bold hover:text-blue mt-4 flex items-center gap-1 hover:gap-2 duration-200"
+  alt={alt ? alt : "Read more button" }
 	href={link.type === 'external'
 		? link.externalLink
 		: '/' + link.internalLink.lang + '/' + link.internalLink.slug}
